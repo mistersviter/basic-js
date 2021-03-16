9e14198ad8b7a07bd51bda18fbef7c1857105d46
Tarabrin Evgeniy <mistersviter@gmail.com>
2021-03-17 01:23:43 +0400

    feat: add catsCount function & dreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    let codeName = '';
+    let strFound = false;
+    if (Array.isArray(members)) {
+        for (let element of members) {
+            if (typeof (element) === 'string') {
+                strFound = true;
+                let convElement = element.trim().toUpperCase();
+                codeName += convElement[0];
+            }
+        }
+    }
+    return strFound ? codeName.split('').sort().join('') : false;
+}
