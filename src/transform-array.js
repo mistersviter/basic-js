8800e9c5307283c30026f60f45c4bb17afcf48e4
Tarabrin Evgeniy <mistersviter@gmail.com>
2021-03-24 21:44:07 +0400

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        let newArr = arr.slice();
+        newArr.map((val, idx, newArr) => {
+            if (val === '--discard-next') {
+                if (newArr[idx + 1] !== undefined) {
+                    newArr.splice(idx, 2, '')
+                } else newArr.splice(idx, 1, '');
+            }
+            if (val === '--discard-prev') {пше
+                if (newArr[idx - 1] !== undefined) {
+                    newArr.splice(idx -  1, 2, '')
+                } else newArr.splice(idx, 1, '');
+            }
+            if (val === '--double-next') {
+                if (newArr[idx + 1] !== undefined) {
+                    newArr.splice(idx, 1, newArr[idx + 1])
+                } else newArr.splice(idx, 1, '');
+            }
+            if (val === '--double-prev') {
+                if (newArr[idx - 1] !== undefined) {
+                    newArr.splice(idx, 1, newArr[idx - 1])
+                } else newArr.splice(idx, 1, '');
+            }
+        });
+        newArr = newArr.filter((element) => element !== '');
+        return newArr;
+    } else throw Error;
+}
